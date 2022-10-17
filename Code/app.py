0a6ebd4840591c863876e6360685dc6ce7443d1b
Chris <christabel.rebello@northwestern.edu>
2022-10-17 01:32:07 -0500

    add updated python notebook,app.js,app.py

--- Code/app.py
+++ Code/app.py
@@ -13,10 +13,17 @@ from flask_cors import CORS
 # Database Setup
 #################################################
 engine = create_engine("sqlite:///../Data/exchange_rates.sqlite")
+engine2 = create_engine("sqlite:///../Data/avgCurnCountry.sqlite")
+connection = engine2.connect()
+resultColumn = connection.execute(f"SELECT * FROM avgCurnCountry")
+resultColumn =["y1998", "y1999","y2000", "y2001", "y2002", "y2003", 
+                "y2004", "y2005", "y2006", "y2007","y2008","y2009", 
+                "y2010", "y2011", "y2012", "y2013","y2014", "y2015", 
+                "y2016", "y2017", "y2018", "y2019", "y2020", "y2021","y2022"]
 
 # reflect an existing database into a new model
 Base = declarative_base()
-
+#Create your class
 class ExchangeRates(Base):
     __tablename__ = 'exchange_rates'
 
@@ -38,6 +45,38 @@ class ExchangeRates(Base):
 # produce relationships
 Base.metadata.create_all(engine)
 
+#Create second class
+class avgCurrency(Base):
+    __tablename__ = 'avgCurnCountry'
+    id = Column(Integer, primary_key=True)
+    country = Column(String)
+    y1998=Column(Float)
+    y1999=Column(Float)
+    y2000=Column(Float)
+    y2001=Column(Float)
+    y2002=Column(Float)
+    y2003=Column(Float)
+    y2004=Column(Float)
+    y2005=Column(Float)
+    y2006=Column(Float)
+    y2007=Column(Float)
+    y2008=Column(Float)
+    y2009=Column(Float)
+    y2010=Column(Float)
+    y2011=Column(Float)
+    y2012=Column(Float)
+    y2013=Column(Float)
+    y2014=Column(Float)
+    y2015=Column(Float)
+    y2016=Column(Float)
+    y2017=Column(Float)
+    y2018=Column(Float)
+    y2019=Column(Float)
+    y2020=Column(Float)
+    y2021=Column(Float)
+    y2022=Column(Float)
+# produce relationships
+Base.metadata.create_all(engine2)
 
 # # reflect the tables
 # Base.prepare(engine, reflect=True)
@@ -148,67 +187,283 @@ def data_json():
 
 @app.route("/api/v1.0/time-series")
 def time_series_data_json():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
+    session = Session(engine2)
+    # Query table
+    resultColumnheader =resultColumn
+    results_AUD = session.query(avgCurrency.y1998,
+                            avgCurrency.y1999,
+                            avgCurrency.y2000,
+                            avgCurrency.y2001,
+                            avgCurrency.y2002,
+                            avgCurrency.y2003,
+                            avgCurrency.y2004,
+                            avgCurrency.y2005,
+                            avgCurrency.y2006,
+                            avgCurrency.y2007,
+                            avgCurrency.y2008,
+                            avgCurrency.y2009,
+                            avgCurrency.y2010,
+                            avgCurrency.y2011,
+                            avgCurrency.y2012,
+                            avgCurrency.y2013,
+                            avgCurrency.y2014,
+                            avgCurrency.y2015,
+                            avgCurrency.y2016,
+                            avgCurrency.y2017,
+                            avgCurrency.y2018,
+                            avgCurrency.y2019,
+                            avgCurrency.y2020,
+                            avgCurrency.y2021,
+                            avgCurrency.y2022).filter(avgCurrency.country == "AUD").all()
+    results_CAD = session.query(avgCurrency.y1998,
+                            avgCurrency.y1999,
+                            avgCurrency.y2000,
+                            avgCurrency.y2001,
+                            avgCurrency.y2002,
+                            avgCurrency.y2003,
+                            avgCurrency.y2004,
+                            avgCurrency.y2005,
+                            avgCurrency.y2006,
+                            avgCurrency.y2007,
+                            avgCurrency.y2008,
+                            avgCurrency.y2009,
+                            avgCurrency.y2010,
+                            avgCurrency.y2011,
+                            avgCurrency.y2012,
+                            avgCurrency.y2013,
+                            avgCurrency.y2014,
+                            avgCurrency.y2015,
+                            avgCurrency.y2016,
+                            avgCurrency.y2017,
+                            avgCurrency.y2018,
+                            avgCurrency.y2019,
+                            avgCurrency.y2020,
+                            avgCurrency.y2021,
+                            avgCurrency.y2022).filter(avgCurrency.country == "CAD").all()
+    results_EUR = session.query(avgCurrency.y1998,
+                            avgCurrency.y1999,
+                            avgCurrency.y2000,
+                            avgCurrency.y2001,
+                            avgCurrency.y2002,
+                            avgCurrency.y2003,
+                            avgCurrency.y2004,
+                            avgCurrency.y2005,
+                            avgCurrency.y2006,
+                            avgCurrency.y2007,
+                            avgCurrency.y2008,
+                            avgCurrency.y2009,
+                            avgCurrency.y2010,
+                            avgCurrency.y2011,
+                            avgCurrency.y2012,
+                            avgCurrency.y2013,
+                            avgCurrency.y2014,
+                            avgCurrency.y2015,
+                            avgCurrency.y2016,
+                            avgCurrency.y2017,
+                            avgCurrency.y2018,
+                            avgCurrency.y2019,
+                            avgCurrency.y2020,
+                            avgCurrency.y2021,
+                            avgCurrency.y2022).filter(avgCurrency.country == "EUR").all()
+    results_JPY = session.query(avgCurrency.y1998,
+                            avgCurrency.y1999,
+                            avgCurrency.y2000,
+                            avgCurrency.y2001,
+                            avgCurrency.y2002,
+                            avgCurrency.y2003,
+                            avgCurrency.y2004,
+                            avgCurrency.y2005,
+                            avgCurrency.y2006,
+                            avgCurrency.y2007,
+                            avgCurrency.y2008,
+                            avgCurrency.y2009,
+                            avgCurrency.y2010,
+                            avgCurrency.y2011,
+                            avgCurrency.y2012,
+                            avgCurrency.y2013,
+                            avgCurrency.y2014,
+                            avgCurrency.y2015,
+                            avgCurrency.y2016,
+                            avgCurrency.y2017,
+                            avgCurrency.y2018,
+                            avgCurrency.y2019,
+                            avgCurrency.y2020,
+                            avgCurrency.y2021,
+                            avgCurrency.y2022).filter(avgCurrency.country == "JPY").all()
+    results_NZD = session.query(avgCurrency.y1998,
+                            avgCurrency.y1999,
+                            avgCurrency.y2000,
+                            avgCurrency.y2001,
+                            avgCurrency.y2002,
+                            avgCurrency.y2003,
+                            avgCurrency.y2004,
+                            avgCurrency.y2005,
+                            avgCurrency.y2006,
+                            avgCurrency.y2007,
+                            avgCurrency.y2008,
+                            avgCurrency.y2009,
+                            avgCurrency.y2010,
+                            avgCurrency.y2011,
+                            avgCurrency.y2012,
+                            avgCurrency.y2013,
+                            avgCurrency.y2014,
+                            avgCurrency.y2015,
+                            avgCurrency.y2016,
+                            avgCurrency.y2017,
+                            avgCurrency.y2018,
+                            avgCurrency.y2019,
+                            avgCurrency.y2020,
+                            avgCurrency.y2021,
+                            avgCurrency.y2022).filter(avgCurrency.country == "NZD").all()
+    results_NOK = session.query(avgCurrency.y1998,
+                            avgCurrency.y1999,
+                            avgCurrency.y2000,
+                            avgCurrency.y2001,
+                            avgCurrency.y2002,
+                            avgCurrency.y2003,
+                            avgCurrency.y2004,
+                            avgCurrency.y2005,
+                            avgCurrency.y2006,
+                            avgCurrency.y2007,
+                            avgCurrency.y2008,
+                            avgCurrency.y2009,
+                            avgCurrency.y2010,
+                            avgCurrency.y2011,
+                            avgCurrency.y2012,
+                            avgCurrency.y2013,
+                            avgCurrency.y2014,
+                            avgCurrency.y2015,
+                            avgCurrency.y2016,
+                            avgCurrency.y2017,
+                            avgCurrency.y2018,
+                            avgCurrency.y2019,
+                            avgCurrency.y2020,
+                            avgCurrency.y2021,
+                            avgCurrency.y2022).filter(avgCurrency.country == "NOK").all()                           
+    results_SEK = session.query(avgCurrency.y1998,
+                            avgCurrency.y1999,
+                            avgCurrency.y2000,
+                            avgCurrency.y2001,
+                            avgCurrency.y2002,
+                            avgCurrency.y2003,
+                            avgCurrency.y2004,
+                            avgCurrency.y2005,
+                            avgCurrency.y2006,
+                            avgCurrency.y2007,
+                            avgCurrency.y2008,
+                            avgCurrency.y2009,
+                            avgCurrency.y2010,
+                            avgCurrency.y2011,
+                            avgCurrency.y2012,
+                            avgCurrency.y2013,
+                            avgCurrency.y2014,
+                            avgCurrency.y2015,
+                            avgCurrency.y2016,
+                            avgCurrency.y2017,
+                            avgCurrency.y2018,
+                            avgCurrency.y2019,
+                            avgCurrency.y2020,
+                            avgCurrency.y2021,
+                            avgCurrency.y2022).filter(avgCurrency.country == "SEK").all()                           
+    
+    results_CHF = session.query(avgCurrency.y1998,
+                            avgCurrency.y1999,
+                            avgCurrency.y2000,
+                            avgCurrency.y2001,
+                            avgCurrency.y2002,
+                            avgCurrency.y2003,
+                            avgCurrency.y2004,
+                            avgCurrency.y2005,
+                            avgCurrency.y2006,
+                            avgCurrency.y2007,
+                            avgCurrency.y2008,
+                            avgCurrency.y2009,
+                            avgCurrency.y2010,
+                            avgCurrency.y2011,
+                            avgCurrency.y2012,
+                            avgCurrency.y2013,
+                            avgCurrency.y2014,
+                            avgCurrency.y2015,
+                            avgCurrency.y2016,
+                            avgCurrency.y2017,
+                            avgCurrency.y2018,
+                            avgCurrency.y2019,
+                            avgCurrency.y2020,
+                            avgCurrency.y2021,
+                            avgCurrency.y2022).filter(avgCurrency.country == "CHF").all()                         
+                            
+                            
+    results_GBP = session.query(avgCurrency.y1998,
+                            avgCurrency.y1999,
+                            avgCurrency.y2000,
+                            avgCurrency.y2001,
+                            avgCurrency.y2002,
+                            avgCurrency.y2003,
+                            avgCurrency.y2004,
+                            avgCurrency.y2005,
+                            avgCurrency.y2006,
+                            avgCurrency.y2007,
+                            avgCurrency.y2008,
+                            avgCurrency.y2009,
+                            avgCurrency.y2010,
+                            avgCurrency.y2011,
+                            avgCurrency.y2012,
+                            avgCurrency.y2013,
+                            avgCurrency.y2014,
+                            avgCurrency.y2015,
+                            avgCurrency.y2016,
+                            avgCurrency.y2017,
+                            avgCurrency.y2018,
+                            avgCurrency.y2019,
+                            avgCurrency.y2020,
+                            avgCurrency.y2021,
+                            avgCurrency.y2022).filter(avgCurrency.country == "GBP").all() 
 
-    #"""Return a dictionary of all exhcange rates"""
-    # Query all passengers
-    results = session.query(ExchangeRates.id,
-                            ExchangeRates.date,
-                            ExchangeRates.month,
-                            ExchangeRates.year,
-                            ExchangeRates.aud,
-                            ExchangeRates.cad,
-                            ExchangeRates.eur,
-                            ExchangeRates.jpy,
-                            ExchangeRates.nzd,
-                            ExchangeRates.nok,
-                            ExchangeRates.sek,
-                            ExchangeRates.chf,
-                            ExchangeRates.gbp,
-                            ExchangeRates.usd).all()
-
+    results_USD = session.query(avgCurrency.y1998,
+                            avgCurrency.y1999,
+                            avgCurrency.y2000,
+                            avgCurrency.y2001,
+                            avgCurrency.y2002,
+                            avgCurrency.y2003,
+                            avgCurrency.y2004,
+                            avgCurrency.y2005,
+                            avgCurrency.y2006,
+                            avgCurrency.y2007,
+                            avgCurrency.y2008,
+                            avgCurrency.y2009,
+                            avgCurrency.y2010,
+                            avgCurrency.y2011,
+                            avgCurrency.y2012,
+                            avgCurrency.y2013,
+                            avgCurrency.y2014,
+                            avgCurrency.y2015,
+                            avgCurrency.y2016,
+                            avgCurrency.y2017,
+                            avgCurrency.y2018,
+                            avgCurrency.y2019,
+                            avgCurrency.y2020,
+                            avgCurrency.y2021,
+                            avgCurrency.y2022).filter(avgCurrency.country == "USD").all()
     session.close()
+    
+    all_AUD = list(np.ravel(results_AUD))
+    all_CAD = list(np.ravel(results_CAD))
+    all_EUR = list(np.ravel(results_EUR))
+    all_JPY = list(np.ravel(results_JPY))
+    all_NZD = list(np.ravel(results_NZD))
+    all_NOK = list(np.ravel(results_NOK))
+    all_SEK = list(np.ravel(results_SEK))
+    all_CHF = list(np.ravel(results_CHF))
+    all_GBP = list(np.ravel(results_GBP))
+    all_USD = list(np.ravel(results_USD))
 
-    results_dict = {}
-    date_list=[]
-    aud_list = []
-    cad_list = []
-    eur_list = []
-    jpy_list = []
-    nzd_list = []
-    nok_list = []
-    sek_list = []
-    chf_list = []
-    gbp_list = []
-    usd_list = []
 
-    for i in range(len(results)):
-        date_list.append(results[i][1])
-        aud_list.append(results[i][4])
-        cad_list.append(results[i][5])
-        eur_list.append(results[i][6])
-        jpy_list.append(results[i][7])
-        nzd_list.append(results[i][8])
-        nok_list.append(results[i][9])
-        sek_list.append(results[i][10])
-        chf_list.append(results[i][11])
-        gbp_list.append(results[i][12])
-        usd_list.append(results[i][13])
-
-    results_dict['date']=date_list
-    results_dict['aud']=aud_list
-    results_dict['cad']=cad_list
-    results_dict['eur']=eur_list
-    results_dict['jpy']=jpy_list
-    results_dict['nzd']=nzd_list
-    results_dict['nok']=nok_list
-    results_dict['sek']=sek_list
-    results_dict['chf']=chf_list
-    results_dict['gbp']=gbp_list
-    results_dict['usd']=usd_list   
-
-    return jsonify(results_dict)
+    all_Years = list(np.ravel(resultColumnheader))
+    #Create a dictionary from the row data and append to a list of all_year
+    #create list 
+    #all_years =[]
+    #for year in results:
+    return jsonify(all_Years,all_AUD,all_CAD,all_EUR,all_JPY,all_NZD,all_NOK,all_SEK,all_CHF,all_GBP,all_USD)
 
 @app.route("/api/v1.0/cad")
 def cad():
--- static/app.js
+++ static/app.js
@@ -1,39 +1,145 @@
 const base_url = "http://127.0.0.1:5000/api/v1.0/"
 const bydate_url = "http://127.0.0.1:5000/api/v1.0/by_date"
 const data_url = "http://127.0.0.1:5000/api/v1.0/data"
+const timeseries_url = "http://127.0.0.1:5000/api/v1.0/time-series"
 
 function init() {
   let dropDownMenu = d3.select("#selDataset");
-
-  d3.json(data_url).then((data) => {
+  d3.json(data_url).then((data) => 
+  {
     console.log(data[0]['date']);
     data.forEach((datum) => {
-      dropDownMenu.append("option").text(datum.date).property("value",datum.date)
+        dropDownMenu.append("option").text(datum.date).property("value", datum.date)
     });
 
     let initDate = data[0]['date'];
     buildBar(initDate);
     buildBubble(initDate);
     buildTimeSeries();
+    
   });
-};
 
+  // timeseries
+   //buildTimeSeries();
+
+   
+};
+/////////////////////////////////////////////////////////////////////////////////
 function buildBar(date) { 
   d3.json(bydate_url).then((data) => {
   
-    Plotly.newPlot("bar", data1, layout1);  
+    //Plotly.newPlot("bar", data, layout1);  
   });
 };
-
+//////////////////////////////////////////////////////////////////////////////////
 function buildBubble(date) {
   d3.json(bydate_url).then((data) => {
 
     // Plotly.newPlot("bubble",data2,layout2);
   });
 };
+//////////////////////////////////////////////////////////////////////////////////
 
-function buildTimeSeries() {
+var dict = {}
+function buildTimeSeries()
+{
+    d3.json(timeseries_url).then((data_2) => {
+        console.log(data_2)
+        // let ts = {}
 
+        var aud = {
+            x: data_2[0],
+            y: data_2[1],
+            mode: 'lines+markers',
+            name: 'Australian dollar'
+        };
+
+        var cad = {
+            x: data_2[0],
+            y: data_2[2],
+            mode: 'lines+markers',
+            name: 'Canadian dollar'
+        };
+
+        var eur = {
+            x: data_2[0],
+            y: data_2[3],
+            mode: 'lines+markers',
+            name: 'Euro'
+        };
+
+        var jpy = {
+            x: data_2[0],
+            y: data_2[4],
+            mode: 'lines+markers',
+            name: 'Japanese yen'
+        };
+        var nzd = {
+            x: data_2[0],
+            y: data_2[5],
+            mode: 'lines+markers',
+            name: 'New Zealand dollar'
+            
+        };
+        var nok = {
+            x: data_2[0],
+            y: data_2[6],
+            mode: 'lines+markers',
+            name: 'Norwegian krone'
+        };
+        var sek = {
+            x: data_2[0],
+            y: data_2[7],
+            mode: 'lines+markers',
+            name: 'Swedish krona'
+        };
+        var chf = {
+            x: data_2[0],
+            y: data_2[8],
+            mode: 'lines+markers',
+            name: 'Swiss franc'
+        };
+        var cbp = {
+            x: data_2[0],
+            y: data_2[9],
+            mode: 'lines+markers',
+            name: 'U.K. pound'
+        };
+
+        var usd = {
+            x: data_2[0],
+            y: data_2[10],
+            mode: 'lines+markers',
+            name: 'U.S. dollar'
+        };
+        var data = [aud, cad, eur, jpy, nzd, nok, sek, chf, cbp, usd];
+
+        var layout = {
+            title: 'Averagr Exchange Currency Rate per Year [1998-2022]',
+            xaxis: {
+                title: 'Year',
+                showgrid: false,
+                //zeroline: false
+            },
+            yaxis: {
+                title: 'Average Exchange Rate',
+                showline: false
+            }
+        };
+
+        Plotly.newPlot('myDiv', data,layout);
+        //dictionary for drop down option if created 
+        //["aud"] = au
+        //dict["cad"] = ca
+        //dict["eur"] = eu
+        //dict["nzd"] = nz
+        //dict["nok"] = no
+        //dict["sek"] = sk
+        //dict["chf"] = ch
+        //dict["cbp"] = cb
+        //dict["usd"] = us
+        
+    });
 };
 
 function optionChanged(date) {
@@ -41,5 +147,8 @@ function optionChanged(date) {
   buildBubble(date);
 };
 
+function optionChanged2() {
+    buildTimeSeries();
+};
 
 init();
